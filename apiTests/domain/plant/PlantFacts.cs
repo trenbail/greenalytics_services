d4a39b23e6218c83479e60a64ff404dae8a56428
zachary lefevre <zachlefevre@gmail.com>
2019-02-24 21:07:25 -0600

    added requirement tests

--- apiTests/domain/plant/PlantFacts.cs
+++ apiTests/domain/plant/PlantFacts.cs
@@ -1,5 +1,7 @@
 using api.domain;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace apiTests
@@ -68,6 +70,67 @@ namespace apiTests
         }
         public class TheReasonsForIncompatibilityMethod : IDisposable
         {
+            private Plant _plant1;
+            private Plant _plant2;
+
+            public TheReasonsForIncompatibilityMethod()
+            {
+                _plant1 = new Plant("test plant1");
+                _plant2 = new Plant("test plant2");
+            }
+
+            public class MockRequirement : IPlantRequirement
+            {
+                public bool ShouldPass;
+                public MockRequirement(bool shouldpass)
+                {
+                    this.ShouldPass = shouldpass;
+                }
+
+                public bool Verify(Plant plant1, Plant plant2)
+                {
+                    return ShouldPass;
+                }
+            }
+            [Fact]
+            public void Test_SinglePassingRequirement_ShouldReturnEmptyList()
+            {
+                _plant1.AddRequirement(new MockRequirement(true));
+                List<IPlantRequirement> failed_requirements = _plant1.ReasonsForIncompatibility(_plant2);
+                Assert.Empty(failed_requirements);
+            }
+            [Fact]
+            public void Test_MultiplePassingRequirement_ShouldReturnEmptyList()
+            {
+                _plant1.AddRequirement(new MockRequirement(true));
+                _plant1.AddRequirement(new MockRequirement(true));
+                List<IPlantRequirement> failed_requirements = _plant1.ReasonsForIncompatibility(_plant2);
+                Assert.Empty(failed_requirements);
+            }
+            [Fact]
+            public void Test_FailingRequirement_ShouldReturnSingleElementList()
+            {
+                _plant1.AddRequirement(new MockRequirement(false));
+                List<IPlantRequirement> failed_requirements = _plant1.ReasonsForIncompatibility(_plant2);
+                Assert.Single(failed_requirements);
+                List<MockRequirement> failed_mock_requirements = failed_requirements.Cast<MockRequirement>().ToList();
+                Assert.False(failed_mock_requirements.First().ShouldPass);
+            }
+            [Fact]
+            public void Test_MultipleFailingRequirement_ShouldReturnSingleElementList()
+            {
+                _plant1.AddRequirement(new MockRequirement(false));
+                _plant1.AddRequirement(new MockRequirement(false));
+                List<IPlantRequirement> failed_requirements = _plant1.ReasonsForIncompatibility(_plant2);
+                Assert.NotEmpty(failed_requirements);
+                Assert.True(failed_requirements.Count == 2);
+                List<MockRequirement> failed_mock_requirements = failed_requirements.Cast<MockRequirement>().ToList();
+                foreach (MockRequirement req in failed_requirements)
+                {
+                    Assert.False(req.ShouldPass);
+                }
+            }
+
             public void Dispose()
             {
             }
