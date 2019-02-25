a8c0642d0c1508fb870a61faf65b1cca42c6830f
zachary lefevre <zachlefevre@gmail.com>
2019-02-24 21:22:25 -0600

    plant facts constructor tested

---
+++ apiTests/domain/PlantGroup/PlantGroupFacts.cs
@@ -0,0 +1,33 @@
+﻿using api.domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace apiTests.domain
+{
+    public class PlantGroupFacts
+    {
+        public class ThePlantGroupConstructor : IDisposable
+        {
+            [Fact]
+            public void Test_NullName_ShouldThrow()
+            {
+                Assert.Throws<ArgumentException>(() => new PlantGroup(null));
+            }
+            [Fact]
+            public void Test_EmptyName_ShouldThrow()
+            {
+                Assert.Throws<ArgumentException>(() => new PlantGroup(""));
+            }
+            [Fact]
+            public void Test_EmptyGuid_ShouldThrow()
+            {
+                Assert.Throws<ArgumentException>(() => new PlantGroup("name", Guid.Empty));
+            }
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
--- apiTests/domain/plant/PlantFacts.cs
+++ apiTests/domain/plant/PlantFacts.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
-namespace apiTests
+namespace apiTests.domain
 {
     public class PlantTests
     {
