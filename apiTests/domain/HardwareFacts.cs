5ae014c9c4f99d26e90f09c3929cb519b10da3fd
zachary lefevre <zachlefevre@gmail.com>
2019-02-25 10:01:09 -0600

    created hardware facts

---
+++ apiTests/domain/HardwareFacts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace apiTests.domain
+{
+    class HardwareFacts
+    {
+        public class TheHardwareConstructorFacts : IDisposable
+        {
+            public void Dispose()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
