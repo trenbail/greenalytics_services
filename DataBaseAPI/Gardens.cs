34e21b1e8bc116df6af5dcd17eb337b2ac7340fd
Ubuntu <ubuntu@ip-172-31-36-167.us-east-2.compute.internal>
2019-03-29 02:34:23 +0000

    Added database project

---
+++ DataBaseAPI/Gardens.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+class Gardens : Connect
+{
+    //Constructor call to base case - selecting 'u_gardens' database
+    public Gardens() : base("u_gardens") { }
+
+    //This function needs to be called after each query function to close the connection
+    public new void Close() { base.Close(); }
+
+    //public wrapper for 'Select * FROM __' in SelectALL in Connection - return type may need to be changed after talking to Zack
+    public MySqlDataReader ShowAll(string tableName) { return SelectAll(tableName); }
+
+}
