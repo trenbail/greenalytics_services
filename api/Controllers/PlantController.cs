57432e38620b0d1ed51399f4ec5957e4c9c1d1bf
zachary lefevre <zachlefevre@gmail.com>
2019-03-28 20:28:15 -0500

    deleted template

--- api/Controllers/PlantController.cs
+++ api/Controllers/PlantController.cs
@@ -145,18 +145,5 @@ namespace api.Controllers
         }
 
 
-
-        #endregion
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
