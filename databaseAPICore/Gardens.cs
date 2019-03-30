80bafcd0ed88ef3de82206083a4e98c8198bc1d8
cdman678 <calebthing@gmail.com>
2019-03-30 14:54:51 -0500

    added AddPlant

--- databaseAPICore/Gardens.cs
+++ databaseAPICore/Gardens.cs
@@ -8,7 +8,7 @@ namespace db.gardens
     class Gardens : Connect
     {
         //Constructor call to base case - selecting 'u_gardens' database
-        public Gardens() : base("u_gardens") { }
+        public Gardens() : base("plants") { }
 
         //This function needs to be called after each query function to close the connection
         public new void Close() { base.Close(); }
---
+++ databaseAPICore/Groups.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using db.connections;
+using MySql.Data.MySqlClient;
+using db.plants;
+
+namespace db.groups
+{
+
+    class Groups : Connect
+    {
+        //Constructor call to base case - selecting 'u_gardens' database
+        public Groups() : base("plants") { }
+
+        //This function needs to be called after each query function to close the connection
+        public new void Close() { base.Close(); }
+
+        //public wrapper for 'Select * FROM __' in SelectALL in Connection - return type may need to be changed after talking to Zack
+        public MySqlDataReader ShowAll(string tableName) { return SelectAll(tableName); }
+
+        public void AddPlant(string userID, string gardenID, string plantName)
+        {
+            //converting to plantID - doing this to allow adding by plantName
+            Plants tempPlant = new Plants();
+            string plantID = tempPlant.Convert(plantName);
+
+            //constructing query
+            string query = String.Format("INSERT INTO hasPlants(userID, groupID, plantID) VALUES({0},{1},{2}) ON DUPLICATE KEY UPDATE quantity = quantity + 1", userID, gardenID, plantName);
+
+            //Open connection
+            Open();
+
+            //Create Command
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            cmd.ExecuteNonQuery();
+
+            Close();
+        }
+
+
+    }
+}
