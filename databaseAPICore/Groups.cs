73e79de12b4425ec34d0ba396fc200afde32bb51
cdman678 <calebthing@gmail.com>
2019-03-30 18:16:14 -0500

    Groups functions
    added functions for garden groups

--- databaseAPICore/Groups.cs
+++ databaseAPICore/Groups.cs
@@ -3,6 +3,7 @@ using db.connections;
 using MySql.Data.MySqlClient;
 using System.Collections.Generic;
 using db.plants;
+using db.gardens;
 
 namespace db.groups
 {
@@ -18,14 +19,63 @@ namespace db.groups
         //public wrapper for 'Select * FROM __' in SelectALL in Connection - return type may need to be changed after talking to Zack
         public MySqlDataReader ShowAll(string tableName) { return SelectAll(tableName); }
 
+        public string Convert(string userID, string name)
+        {
+
+            //constructing query
+            string query = String.Format("SELECT groupID FROM hasGroups WHERE userID = '{0}' AND groupName = '{1}';", userID, name);
+
+            //Open connection
+            Open();
+
+            //Create Command
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+
+            //Create a data reader and Execute the command
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+
+            string returnString = string.Empty;
+
+            while (dataReader.Read())
+            {
+                returnString = dataReader.GetString("groupID");
+            }
+
+            Close();
+
+            return returnString;
+        }
+
+        public void AddGroup(string userID, string gardenName, Guid groupID_g, string groupName)
+        {
+            //convert garden name to gardenID
+            Gardens tempgarden = new Gardens();
+            string gardenID = tempgarden.Convert(userID, gardenName);
+            //convert groupID_g to string
+            string groupID = groupID_g.ToString();
+
+            //constructing query
+            string query = String.Format("INSERT INTO hasGroups VALUES('{0}','{1}','{2}','{3}');", userID, gardenID, groupID, groupName); 
+
+            //Open connection
+            Open();
+
+            //Create Command
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            cmd.ExecuteNonQuery();
+
+            Close();
+        }
 
         //This function adds the given plant to the given garden group for the given user
-        public void AddPlant(string userID, Guid groupID_g, string plantName)
+        public void AddPlant(string userID, string groupName, string plantName)
         {
-            string groupID = groupID_g.ToString();
             //converting to plantID - doing this to allow adding by plantName
             Plants tempPlant = new Plants();
             string plantID = tempPlant.Convert(plantName);
+
+            //Convert groupName to groupID given the user
+            string groupID = Convert(userID, groupName);
 
             //constructing query - adds the plant to the group or if it is already there increases count by one
             string query = String.Format("INSERT INTO hasPlants(userID, groupID, plantID) VALUES('{0}','{1}',{2}) ON DUPLICATE KEY UPDATE quantity = quantity + 1", userID, groupID, plantID);
@@ -40,10 +90,11 @@ namespace db.groups
             Close();
         }
 
-        public List<string> ListGroup(string userID, Guid groupID_g)
+        //This function returns a list of plant names inside a group
+        public List<string> ListGroup(string userID, string groupName)
         {
             //constructing query
-            string groupID = groupID_g.ToString();
+            string groupID = Convert(userID, groupName);
             string query = String.Format("SELECT p.name FROM masterPlants p, hasPlants h WHERE p.plantID = h.plantID AND userID = '{0}';", groupID);
 
             //Open connection
@@ -61,9 +112,11 @@ namespace db.groups
             {
                 returnList.Add(dataReader.GetString("name"));
             }
+
             Close();
 
             return (returnList);
         }
+
     }
 }
